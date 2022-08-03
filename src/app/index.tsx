e5138fc41440ff61293d49a2fcbb43fe3b8f4a79
aetheryx <zainalih3c@gmail.com>
2022-08-03 06:53:38 +0200

    add monkey game

---
+++ src/app/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Button } from './button';
+import { Client } from './client';
+
+const client = new Client();
+
+const createCircle = (color: string, text: string = ''): JSX.Element => (
+  <svg height={100} width={100}>
+    <circle cx={50} cy={50} r={40} fill={color} />
+    <text x="50%" y="67%" textAnchor="middle" fontSize={50}>
+      {text}
+    </text>
+  </svg>
+);
+
+let startedAt: number;
+let buttons: Array<{ button: Button; index: number }>;
+
+client.on('keyDown', (button) => {
+  startedAt ??= Date.now();
+
+  if (buttons.length === 0 && button.coordinates.x === 2 && button.coordinates.y === 1) {
+    button.setTitle('');
+    return main();
+  }
+
+  if (button.index !== buttons[0].button.index) {
+    const { index } = buttons.find(b => b.button.index === button.index);
+    return button.setImage(createCircle('red', index.toString()));
+  }
+
+  if (buttons.shift().index !== client.buttons.length) {
+    return button.setImage(createCircle('green'));
+  }
+
+  onFinish();
+});
+
+function main(): void {
+  startedAt = null;
+  buttons = [ ...client.buttons ]
+    .sort(() => Math.random() - 0.5)
+    .map((button, index) => ({ button, index: index + 1 }));
+
+  for (const { index, button } of buttons) {
+    button.setImage(createCircle('white', index.toString()));
+  }
+}
+
+function onFinish(): void {
+  for (const button of client.buttons) {
+    button.setImage(<svg height={100} width={100} />);
+  }
+
+  const duration = Date.now() - startedAt;
+  client.getButton({ x: 2, y: 1 }).setTitle(`${(duration / 1000).toFixed(2)}s`);
+}
+
+setTimeout(main, 1000);
